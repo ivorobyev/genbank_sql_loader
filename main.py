752ba300c681f553ce8d2ec00e7af27e622c2c86
ivorobyev <vorobyevilia@gmail.com>
2019-12-25 17:38:30 +0300

    exceptions

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ with open('taxons_full_list.txt', 'r') as f:
 print('All taxons: {0}'.format(len(taxons)))
 counter = 0
 for t in taxons:
-    gl.put_genbank_data_to_db('genbank.db', t, 170)
-    counter += 1
-    print('Processed {0} from {1} taxons'.format(counter, len(taxons)))
-    print('----------------------')
+    try:
+        gl.put_genbank_data_to_db('genbank.db', t, 100)
+    except:
+        print('Error')
+    finally:
+        counter += 1
+        print('Processed {0} from {1} taxons'.format(counter, len(taxons)))
+        print('----------------------')
+
 print('Done {0}'.format(time.time() - start_time))
